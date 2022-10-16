85a0dcb60019861140302166fa2276b5a29598f7
User <User@5114001660TI778>
2022-10-16 19:26:58 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_For01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入三角形長度：");
+            string thelength = Console.ReadLine();
+
+            for (int i = int.Parse(thelength); i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+
+            Console.Read();
+        }
+    }
+}
